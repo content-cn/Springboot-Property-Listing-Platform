6c8c0c41b2214cb8d5f99d2b5031a07e79fcbd60
om-weye <omraj.sharma@wheelseye.com>
2025-01-11 11:51:05 +0530

    first commit

---
+++ src/main/java/com/example/awaas/AwaasVishwaApplication.java
@@ -0,0 +1,13 @@
+package com.example.awaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwaasVishwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwaasVishwaApplication.class, args);
+	}
+
+}
